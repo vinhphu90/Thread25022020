4e7a802c5be6f38a7de2b0f691e7bcc00e08cbc4
vinhphu90 <61481544+vinhphu90@users.noreply.github.com>
2020-05-12 20:29:22 +0700

    thread countDownTimer

--- app/src/main/java/com/examplevinhphutvp/thread25022020/MainActivity.java
+++ app/src/main/java/com/examplevinhphutvp/thread25022020/MainActivity.java
@@ -3,6 +3,9 @@ package com.examplevinhphutvp.thread25022020;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // khoi tao ra luong
+        final Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Log.d("BBB","Hello main");
+            }
+        });
+        thread.start();
+        CountDownTimer countDownTimer = new CountDownTimer(1000,1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                Log.d("BBB",thread.getState().name());
+
+            }
+        };
+        countDownTimer.start();
+
+
     }
 }
