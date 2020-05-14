4bf91de7fb0070a32272361bed9bf144b7390985
vinhphu90 <61481544+vinhphu90@users.noreply.github.com>
2020-05-14 23:49:54 +0700

    finish

--- app/src/main/java/com/examplevinhphutvp/thread25022020/MainActivity.java
+++ app/src/main/java/com/examplevinhphutvp/thread25022020/MainActivity.java
@@ -9,7 +9,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     int A,B,C ;
-
+    MyFlag myFlag ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,38 +19,79 @@ public class MainActivity extends AppCompatActivity {
 //        A sinh ra 1 so index
 //        B sinh ra 1 so index
 //        C ket qua a +b
-        A=B=C =0 ;
+        A=B=C = 0 ;
+        myFlag = new MyFlag(0);
         Thread threadA = new Thread(new Runnable() {
             @Override
             public void run() {
-                for (int i = 1 ; i <= 10 ; i ++){
-                    A = i ;
-                    Log.d("BBB","A :" + i);
+
+                    synchronized (myFlag){
+                        for (int i = 1 ; i <= 10 ; ){
+                            if (myFlag.position == 0){
+                                A = i;
+                                Log.d("BBB","A : " + i);
+                                myFlag.position = 1;
+                                i++;
+                                myFlag.notifyAll();
+                            }else{
+                                try {
+                                    myFlag.wait();
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
+                    }
+
                 }
+            });
+            Thread threadB = new Thread(new Runnable() {
+                @Override
+                public void run() {
 
-            }
-        });
-        Thread threadB = new Thread(new Runnable() {
-            @Override
-            public void run() {
+                        synchronized (myFlag){
+                            for (int i = 1 ; i <= 10 ;){
+                                if (myFlag.position == 1){
+                                    B = i;
+                                    Log.d("BBB","B : " + i);
+                                    myFlag.position = 2;
+                                    i++;
+                                    myFlag.notifyAll();
+                                }else{
+                                    try {
+                                        myFlag.wait();
+                                    } catch (InterruptedException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            }
+                        }
+                    }
+                });
+                Thread threadC = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
 
-                for (int i = 1 ; i <= 10 ; i ++){
-                    B = i;
-                    Log.d("BBB","B :" + i);
-                }
+                            synchronized (myFlag){
+                                for (int i = 1 ; i <= 10 ;){
+                                    if (myFlag.position == 2){
+                                        C = A + B;
+                                        Log.d("BBB","C : " + C);
+                                        myFlag.position = 0;
+                                        i++;
+                                        myFlag.notifyAll();
+                                    }else{
+                                        try {
+                                            myFlag.wait();
+                                        } catch (InterruptedException e) {
+                                            e.printStackTrace();
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    });
 
-            }
-        });
-        Thread threadC = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 1 ; i <= 10 ; i ++){
-                    C = A + B;
-                    Log.d("BBB","C :" + i);
-                }
-
-            }
-        });
         threadA.start();
         threadB.start();
         threadC.start();
---
+++ app/src/main/java/com/examplevinhphutvp/thread25022020/MyFlag.java
@@ -0,0 +1,9 @@
+package com.examplevinhphutvp.thread25022020;
+
+public class MyFlag {
+    int position ;
+
+    public MyFlag(int position) {
+        this.position = position;
+    }
+}
